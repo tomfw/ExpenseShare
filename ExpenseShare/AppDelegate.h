8106dba7b530af676b0374e43729826c1d2078eb
Thomas Williams <thomasforsythewilliams@gmail.com>
2014-11-02 01:03:30 -0400

    Initial Commit

---
+++ ExpenseShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExpenseShare
+//
+//  Created by Thomas Williams on 11/2/14.
+//  Copyright (c) 2014 Thomas Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
